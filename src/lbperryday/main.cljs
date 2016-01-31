5d8f1a58a6e58d708146e30325d40146fa2d3219
Rusty Bentley <mycoding.alert@gmail.com>
2016-01-31 10:26:37 -0600

    side specs

--- src/lbperryday/main.cljs
+++ src/lbperryday/main.cljs
@@ -10,61 +10,62 @@
 (defn roll-dice! []
   (println "roll-dice! needs an implementation..."))
 
-(defn dice-side [side-color dots]
-  [:svg
-     {:width 100
-      :height 100}
-     [:rect
-      {:width 74
-       :height 74
-       :fill side-color}]
-     #_[:circle
-      {:cx 37
-       :cy 37
-       :r 27
-       :fill "#F8F1E5"}]
-   (for [dot dots]
-     ^{:key dot}
-     [:circle
-      {:cx (:x dot)
-       :cy (:y dot)
-       :r 6
-       :fill "snow"}])])
+(def dice-sides {:one {:color "cadetblue" :dots [{:x 37 :y 37}]}
+                 :two {:color "coral" :dots [{:x 14.25 :y 57.5}
+                                           {:x 56.75 :y 16.5}]}
+                 :three {:color "cornflowerblue" :dots [{:x 14.25 :y 57.5}
+                                                        {:x 37 :y 37}
+                                                        {:x 56.75 :y 16.5}]}
+                 :four {:color "darkkhaki" :dots [{:x 14.27 :y 16.5}
+                                                  {:x 14.25 :y 57.5}
+                                                  {:x 56.75 :y 16.5}
+                                                  {:x 56.75 :y 57.5}]}
+                 :five {:color "darkslateblue" :dots [{:x 14.27 :y 16.5}
+                                                      {:x 14.25 :y 57.5}
+                                                      {:x 37 :y 37}
+                                                      {:x 56.75 :y 16.5}
+                                                      {:x 56.75 :y 57.5}]}
+                 :six {:color "brown" :dots [{:x 14.27 :y 16.5}
+                                             {:x 14.27 :y 37}
+                                             {:x 14.25 :y 57.5}
+                                             {:x 56.75 :y 16.5}
+                                             {:x 56.75 :y 37}
+                                             {:x 56.75 :y 57.5}]}})
+
+(defn dice-side
+  ([side-color dots]
+   ^{:key side-color}
+   [:svg
+    {:width 100
+     :height 100}
+    [:rect
+     {:width 74
+      :height 74
+      :fill side-color}]
+    (for [dot dots]
+      ^{:key dot}
+      [:circle
+       {:cx (:x dot)
+        :cy (:y dot)
+        :r 6
+        :fill "snow"}])])
+  ([side-spec]
+   (dice-side (:color side-spec) (:dots side-spec))))
 
 (defn main-view []
   [:center
    [:h1 "LBPERRYDAY!"]
    [:div
+    {:id "dice-side-display"}
+    (for [side-spec (vals dice-sides)]
+      (dice-side side-spec))]
+
+   [:div
     {:id "dice-roll-area"
      :on-click (fn [e]
                  (roll-dice!))}
-    (dice-side "cadetblue"
-               [{:x 37 :y 37}])
-    (dice-side "coral"
-               [{:x 14.25 :y 57.5}
-                {:x 56.75 :y 16.5}])
-    (dice-side "cornflowerblue"
-               [{:x 14.25 :y 57.5}
-                {:x 37 :y 37}
-                {:x 56.75 :y 16.5}])
-    (dice-side "darkkhaki"
-               [{:x 14.27 :y 16.5}
-                {:x 14.25 :y 57.5}
-                {:x 56.75 :y 16.5}
-                {:x 56.75 :y 57.5}])
-    (dice-side "darkslateblue"
-               [{:x 14.27 :y 16.5}
-                {:x 14.25 :y 57.5}
-                {:x 37 :y 37}
-                {:x 56.75 :y 16.5}
-                {:x 56.75 :y 57.5}])
-    (dice-side "brown"
-               [{:x 14.27 :y 16.5}
-                {:x 14.27 :y 37}
-                {:x 14.25 :y 57.5}
-                {:x 56.75 :y 16.5}
-                {:x 56.75 :y 37}
-                {:x 56.75 :y 57.5}])]])
+    "make the dice appear as a cube"
+    ]])
 
 (defn ^:export main []
   (when-let [app (. js/document (getElementById "app"))]
