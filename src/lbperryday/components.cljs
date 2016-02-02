8ab841d76b140e2fb3d6ba2a451389d3bf654c99
Rusty Bentley <mycoding.alert@gmail.com>
2016-02-02 12:55:25 -0600

    adding components file

---
+++ src/lbperryday/components.cljs
@@ -0,0 +1,26 @@
+(ns lbperryday.components)
+
+(def text-defaults)
+
+(defn drag-move-fn [on-drag]
+  (fn [evt]
+    (on-drag (.-clientX evt) (.-clientY evt))))
+
+(defn drag-end-fn [drag-move drag-end on-end]
+  (fn [evt]
+    (events/unlisten js/window EventType.MOUSEMOVE drag-move)
+    (events/unlisten js/window EventType.MOUSEUP @drag-end)
+    (on-end)))
+
+(defn dragging
+  ([on-drag] (dragging on-drag (fn []) (fn [])))
+  ([on-drag on-start on-end]
+   (let [drag-move (drag-move-fn on-drag)
+         drag-end-atom (atom nil)
+         drag-end (drag-end-fn drag-move drag-end-atom on-end)]
+     (on-start)
+     (reset! drag-end-atom drag-end)
+     (events/listen js/window EventType.MOUSEMOVE drag-move)
+     (events/listen js/window EventType.MOUSEUP drag-end))))
+
+
