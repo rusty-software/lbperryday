66c74cbb2409df435be39851ab93fdf891097ec6
Rusty Bentley <mycoding.alert@gmail.com>
2016-02-07 11:57:07 -0600

    Publishing update

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,6 +1,7 @@
 lein do clean, cljsbuild once min
 cp resources/public/js/compiled/lbperryday.js js/compiled/
 cp resources/public/css/* css/
+cp resources/public/img/* img/
 git commit -am "Publishing update"
 git push
 
