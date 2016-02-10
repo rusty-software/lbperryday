af55ddaa1a41f60b23c495d51c8ffc883820d21d
Rusty Bentley <mycoding.alert@gmail.com>
2016-02-09 20:41:07 -0600

    add sounds to build script

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,6 +2,7 @@ lein do clean, cljsbuild once min
 cp resources/public/js/compiled/lbperryday.js js/compiled/
 cp resources/public/css/* css/
 cp resources/public/img/* img/
+cp resources/public/sounds/* sounds/
 git commit -am "Publishing update"
 git push
 
