21315e8e41b884dc6c890671f5cd4eaffd3bd887
Rusty Bentley <mycoding.alert@gmail.com>
2016-04-04 08:58:16 -0500

    end game not quite working

--- src/lbperryday/main.cljs
+++ src/lbperryday/main.cljs
@@ -3,7 +3,9 @@
             [lbperryday.auto-init]
             [lbperryday.view.main :as view-main]))
 
-
 (reagent/render-component
   [view-main/play-area]
   (. js/document (getElementById "app")))
+
+(defn on-js-reload []
+  (view-main/play-area))
